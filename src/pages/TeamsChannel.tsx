ea3895b42265fc63f9e40ed95434245605b173e2
lovetojo <toluwaniojof@gmail.com>
2025-06-17 05:32:03 -0700

    last commit

--- src/pages/TeamsChannel.tsx
+++ src/pages/TeamsChannel.tsx
@@ -36,7 +36,6 @@ function TeamsChannel() {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     console.log("Form submitted:", formData)
-    // Add your form submission logic here
   }
 
   return (
